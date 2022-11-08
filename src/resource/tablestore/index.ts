7b6ae0ed6a7fd8a9efbd1a238f9eaf8723042511
heimanba <yuankun.yk@alibaba-inc.com>
2022-11-08 19:48:12 +0800

    generate tablestore instance

--- src/resource/tablestore/index.ts
+++ src/resource/tablestore/index.ts
@@ -1,16 +1,26 @@
 // refer: https://github.com/aliyun/aliyun-tablestore-nodejs-sdk
-import { Client } from 'tablestore';
-import { lodash as _ } from '@serverless-devs/core';
-import { IProps } from '../../common/entity';
-import * as generateDbParams from './generate-db-params';
-import logger from '../../common/logger';
-
+import { Client } from "tablestore";
+import { lodash as _, popCore } from "@serverless-devs/core";
+import { IProps } from "../../common/entity";
+import * as generateDbParams from "./generate-db-params";
+import logger from "../../common/logger";
+import { OTS_INSTANCE_NAME } from "../../constants";
 
 export default class Ots {
   client: Client;
+  popClient: any;
   dbConfig: any;
+  envConfig: any;
 
   constructor(envConfig: IProps) {
+    this.envConfig = envConfig;
+    this.popClient = new popCore({
+      accessKeyId: envConfig.ACCESS_KEY_ID,
+      accessKeySecret: envConfig.ACCESS_KEY_SECRET,
+      endpoint: `https://ots.${envConfig.REGION}.aliyuncs.com`,
+      apiVersion: "2016-06-20",
+    });
+
     this.client = new Client({
       accessKeyId: envConfig.ACCESS_KEY_ID,
       accessKeySecret: envConfig.ACCESS_KEY_SECRET,
@@ -52,24 +62,70 @@ export default class Ots {
     ];
   }
 
+  // 初始化实例
+  async initInstance() {
+    const popClient = this.popClient;
+    const instanceName =
+      this.envConfig?.OTS_INSTANCE_NAME || OTS_INSTANCE_NAME;
+    let isExistInstance = false;
+    logger.info(`Init Ots Instance ${instanceName} Start`);
+    try {
+      await popClient.request(
+        "GetInstance",
+        { InstanceName: instanceName },
+        {
+          method: "GET",
+          formatParams: false,
+        }
+      );
+      isExistInstance = true;
+    } catch (error) {}
+    
+    if (isExistInstance) {
+      logger.debug(`Ots Instance Exist ${instanceName} Exist`);
+      logger.info(`Init Ots Instance ${instanceName} Success`);
+      return;
+    }
+    logger.debug(`Create Instance ${instanceName} Start...`);
+    await popClient.request(
+      "InsertInstance",
+      { InstanceName: instanceName },
+      {
+        method: "POST",
+        formatParams: false,
+      }
+    );
+    logger.info(`Init Ots Instance ${instanceName} Success`);
+  }
+
   async init() {
-    for (const { name, indexName, genTableParams, genIndexParams } of this.dbConfig) {
+    await this.initInstance();
+    for (const { name, indexName, genTableParams, genIndexParams } of this
+      .dbConfig) {
       logger.debug(`handler ${name} start`);
       await this.handlerTable(name, genTableParams(name));
 
       if (indexName) {
-        await this.handlerIndex(name, indexName, genIndexParams(name, indexName));
+        await this.handlerIndex(
+          name,
+          indexName,
+          genIndexParams(name, indexName)
+        );
       }
 
       logger.debug(`handler ${name} end`);
     }
   }
 
-  private async handlerIndex(tableName: string, indexName: string, params: any) {
+  private async handlerIndex(
+    tableName: string,
+    indexName: string,
+    params: any
+  ) {
     logger.debug(`need handler index: ${indexName}`);
     try {
       await this.client.describeSearchIndex({ tableName, indexName });
-      
+
       logger.debug(`check index ${indexName} exist, skip create`);
       return;
     } catch (ex) {
@@ -78,11 +134,16 @@ export default class Ots {
         throw ex;
       }
     }
-    logger.debug(`need create index ${indexName}, params: ${JSON.stringify(params, null, 2)}`);
+    logger.debug(
+      `need create index ${indexName}, params: ${JSON.stringify(
+        params,
+        null,
+        2
+      )}`
+    );
     await this.client.createSearchIndex(params);
     logger.debug(`create index ${indexName} success`);
   }
-
 
   private async handlerTable(tableName: string, params: any): Promise<Boolean> {
     logger.debug(`check table ${tableName}`);
@@ -96,7 +157,13 @@ export default class Ots {
         throw ex;
       }
     }
-    logger.debug(`need create table ${tableName}, params: ${JSON.stringify(params, null, 2)}`);
+    logger.debug(
+      `need create table ${tableName}, params: ${JSON.stringify(
+        params,
+        null,
+        2
+      )}`
+    );
     await this.client.createTable(params);
     logger.debug(`create table ${tableName} success`);
   }
